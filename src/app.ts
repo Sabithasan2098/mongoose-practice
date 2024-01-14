777d1e9b48d5438096c8611339b00051e771115d
Sabithasan2098 <sabithasan2098@gmail.com>
2024-01-14 15:40:51 +0600

    interface&schema

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,50 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { Schema } from "mongoose";
 const app: Application = express();
 
 // using cors
 app.use(cors());
 
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName: string;
+      lastName: string;
+    };
+    dataOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNumber: string;
+    emergencyContactNumber: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+  const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+    name: {
+      firstName: { type: String, required: true },
+      middleName: { type: String, required: true },
+      lastName: { type: String, required: true },
+    },
+    dataOfBirth: { type: String },
+    gender: { type: String, required: true },
+    email: { type: String },
+    contactNumber: { type: String, required: true },
+    emergencyContactNumber: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+  });
 });
 
 export default app;
